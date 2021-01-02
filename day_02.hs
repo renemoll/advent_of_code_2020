4d5807cd9369260f539ef7f11fba4760680d771c
Rene Moll <github@r-moll.nl>
2021-01-02 22:13:50 +0100

    AoC: add solution for day 2

---
+++ day_02.hs
@@ -0,0 +1,28 @@
+import Data.List.Split
+
+parse :: [Char] -> ([Char], Char, Int, Int)
+parse input = (password, check, low, high)
+  where [crit, password] = splitOn ": " input
+        [limits, checks] = splitOn " " crit
+        [lows, highs] = splitOn "-" limits
+        check = head checks
+        low = read lows
+        high = read highs
+
+validateAmount :: ([Char], Char, Int, Int) -> Bool
+validateAmount (pw, c, low, high) = result
+  where t = length $ filter (==c) pw
+        result = (t >= low) && (t <= high)
+
+validatePosition :: ([Char], Char, Int, Int) -> Bool
+validatePosition (pw, c, low, high) = result
+  where result = ((pw !! (low - 1)) == c) /= ((pw !! (high - 1)) == c)
+
+main :: IO ()
+main = do
+  entries <- map parse . lines <$> readFile "day_02_input.txt"
+  let result = sum [if validateAmount entry then 1 else 0 | entry <- entries]
+  print $ "Part 1: " ++ (show result)
+
+  let result2 = sum [if validatePosition entry then 1 else 0 | entry <- entries]
+  print $ "Part 2: " ++ (show result2)
