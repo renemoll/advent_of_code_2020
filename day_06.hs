28ee12b243132b2cc3f62399bd44f7ed7bfe6c9f
Rene Moll <github@r-moll.nl>
2021-01-13 20:25:42 +0100

    AoC: update day 6 with library functions

--- day_06.hs
+++ day_06.hs
@@ -1,26 +1,21 @@
+import Data.List
 import Data.List.Split
 
-uniqueAnswers :: String -> Int
-uniqueAnswers = helper []
-  where helper seen [] = length seen
-        helper seen (x:xs)
-          | x `elem` seen = helper seen xs
-          | otherwise = helper (seen ++ [x]) xs
-
 countAnswers :: [String] -> Int
-countAnswers input = uniqueAnswers $ concat input
+-- from a list of charachters, count the unique ones (nub removes dublicates)
+countAnswers input = (length . nub . concat) input
 
 countAgreedAnswers :: [String] -> Int
-countAgreedAnswers (x:xs) = helper x xs
-  where helper seen [] = uniqueAnswers seen
-        helper seen (x:xs) = helper [c  | c <- x, c `elem` seen] xs
+-- from a list of characters, count the character present in all entries (intersect determines the common elements)
+countAgreedAnswers input = (length . foldl1 intersect) input
 
 main :: IO ()
 main = do
-  groups <- splitOn "\n\n" <$> readFile "day_06_input.txt"
+  -- Split the text into blocks
+  groups <- map lines . splitOn "\n\n" <$> readFile "day_06_input.txt"
 
-  let part1 = sum $ map (countAnswers . lines) groups
+  let part1 = sum $ map countAnswers groups
   print $ "Part 1: " ++ (show part1)
 
-  let part2 = sum $ map (countAgreedAnswers . lines) groups
+  let part2 = sum $ map countAgreedAnswers groups
   print $ "Part 2: " ++ (show part2)
