cebe8aca1ebcae15d9fc54727afb9d3dc21882a1
Rene Moll <github@r-moll.nl>
2021-01-13 20:58:49 +0100

    AoC: update solution for day 1 to avoid do-blocks

--- day_01.hs
+++ day_01.hs
@@ -1,20 +1,14 @@
 
-get2020_2part_product :: FilePath -> IO Int
-get2020_2part_product path = do
-  -- read the file line by line and convert each line to an Int
-  numbers <- map (read :: String -> Int) . lines <$> readFile path
-  let result = head [a*b | a <- numbers, b <- numbers, a + b == 2020]
-  return result
+get2020_2part_product :: [Int] -> Int
+get2020_2part_product numbers = head [a*b | a <- numbers, b <- numbers, a + b == 2020]
 
-get2020_3part_product :: FilePath -> IO Int
-get2020_3part_product path = do
-  numbers <- map (read :: String -> Int) . lines <$> readFile path
-  let result = head [a*b*c | a <- numbers, b <- numbers, c <- numbers, a + b + c == 2020]
-  return result
+get2020_3part_product :: [Int] -> Int
+get2020_3part_product numbers = head [a*b*c | a <- numbers, b <- numbers, c <- numbers, a + b + c == 2020]
 
 main :: IO ()
 main = do
-  result2 <- get2020_2part_product "day_01_input.txt"
-  print result2
-  result3 <- get2020_3part_product "day_01_input.txt"
-  print result3
+  -- read the file line by line and convert each line to an Int
+  numbers <- map (read :: String -> Int) . lines <$> readFile "day_01_input.txt"
+
+  print $ get2020_2part_product numbers
+  print $ get2020_3part_product numbers
